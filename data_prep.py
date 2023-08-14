f347abd33c5de00d8156d27248aae034d83e6643
LeahDZuckerman <LeahDZuckerman@gmail.com>
2023-08-14 08:06:18 -0600

    add train-test split function

---
+++ data_prep.py
@@ -0,0 +1,173 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+import glob
+import pickle
+from sklearn.model_selection import train_test_split
+import os
+
+PATH_DATA_DIR = '../Data/'  # path for the data directory
+PATH_PICKLE = '../Data/'    # path for the pickle file
+IMSIZE_SPLIT = 64           # the size of the split image
+
+def read_tif(save_pickle=True):
+    """
+    Read the tif files and save them as the pickle files (dict)
+
+    parameters:
+        save_pickle: save the pickle files if True (bool)
+
+    outputs:
+        None
+    """
+
+    # path for the filenames
+    dir_path = PATH_DATA_DIR + '**/*.tif'
+    filepaths = glob.glob(dir_path, recursive=True) # list
+    filepaths.sort()
+
+    filenames = []
+    labels = []
+    features = []
+
+    num_images = len(filepaths)
+    num_row, num_col, num_channel = np.array(Image.open(filepaths[0])).shape
+    images = np.zeros((num_images, num_row, num_col, num_channel))
+
+    print('Reading tiff files')
+
+    for i, filepath in enumerate(filepaths):
+        
+        # get the filename, label, and feature
+        filename = filepath.split('/')[-1].split('.')[0] # e.g., sj-03-2810_001
+        label    = filepath.split('/')[-2]               # e.g., CLL
+        feature  = filepath.split('/')[-1].split('_')[0] # e.g., sj-03-2810
+        
+        filenames.append(filename)
+        labels.append(label)
+        features.append(feature)
+        
+        # read the tif file
+        im = Image.open(filepath)
+        imarray = np.array(im)
+        images[i] = imarray
+
+    # save to the dictory
+    lymphoma_dict = {'images': images,
+                     'labels': np.array(labels),
+                     'filenames': np.array(filenames),
+                     'features': np.array(features)
+                    }
+
+    print('Saving to pickle file')
+
+    if save_pickle:
+        # save the dictory as the pickel files
+        with open(PATH_PICKLE+'lymphoma.pickle', 'wb') as file:
+            pickle.dump(lymphoma_dict, file)
+    
+    print('Saved pickle')
+
+    return lymphoma_dict
+
+def trim_and_split_data(save_pickle=True):
+    """
+    Trim and split the images and save them as the pickle files (numpy array)
+
+    parameters:
+        save_pickle: save the pickle files if True (bool)
+
+    outputs:
+        None
+    """
+
+    print('Loading pickle file')
+
+    # Use loads to load the variable
+    with open(PATH_PICKLE+'lymphoma.pickle', 'rb') as file:
+        lymphoma_data = pickle.load(file)
+    print(f'Read the pickel file that with the keys: {list(lymphoma_data.keys())}')
+
+    # get the images
+    images = lymphoma_data['images']
+    num_images, num_rows, num_cols, num_channels = images.shape
+    print(f'Original image shape:{ images.shape}')
+
+    print('Trimming')
+
+    # trim the image
+    imsize_trim  = 1024   # the size of the trimmed image
+    im_trim_startpx = 10  # the start pixel to trim the image
+    image_trim = images[:, 
+                        im_trim_startpx:im_trim_startpx+imsize_trim, 
+                        im_trim_startpx:im_trim_startpx+imsize_trim, 
+                        :]
+    print(f'Trim the image to the shape of: {image_trim.shape}')
+
+    print('Splitting')
+
+    # Split the image to many subimages
+    image_split = np.array([image_trim[i][x:x+IMSIZE_SPLIT, y:y+IMSIZE_SPLIT] 
+                            for i in range(num_images) 
+                            for x in range(0, imsize_trim, IMSIZE_SPLIT) 
+                            for y in range(0, imsize_trim, IMSIZE_SPLIT)])
+    print(f'Split the image to the shape of: {image_split.shape}')
+
+    # match the lables, filenames, features to the split images
+    labels    = lymphoma_data['labels']
+    filenames = lymphoma_data['filenames']
+    features  = lymphoma_data['features']
+
+    # the number of the split subimages in the original trim image
+    num_subimg_in_orgimg = (imsize_trim/IMSIZE_SPLIT)**2 
+
+    labels_split = np.repeat(labels, num_subimg_in_orgimg)
+    filenames_split = np.repeat(filenames, num_subimg_in_orgimg)
+    features_split = np.repeat(features, num_subimg_in_orgimg)
+
+    print('Saving trimed pickles')
+
+    if save_pickle:
+        # save the numpy array as the pickel files
+        with open(PATH_PICKLE+'X_data.pickle', 'wb') as file:
+            pickle.dump(image_split, file)
+            print(f'save the X_data.pickle to {PATH_PICKLE}X_data.pickle')
+
+        with open(PATH_PICKLE+'y_data.pickle', 'wb') as file:
+            pickle.dump(labels_split, file)
+            print(f'save the y_data.pickle to {PATH_PICKLE}y_data.pickle')
+
+        with open(PATH_PICKLE+'X_filename.pickle', 'wb') as file:
+            pickle.dump(filenames_split, file)
+            print(f'save the X_filename.pickle to {PATH_PICKLE}X_filename.pickle')
+
+        with open(PATH_PICKLE+'X_feature.pickle', 'wb') as file:
+            pickle.dump(features_split, file)
+            print(f'save the X_feature.pickle to {PATH_PICKLE}X_feature.pickle')
+
+
+def load_data_from_pickles(imfile, labelfile):
+    '''
+    Process the data in the pickle files into train/test/val sets
+    Save as numpy files
+    '''
+    # Get data from pickles
+    with open(imfile, 'rb') as filename:
+            images = pickle.load(filename)
+    with open(labelfile, 'rb') as filename:
+            labels = pickle.load(filename)
+    # Split into train and test
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=1)
+    np.save(PATH_DATA_DIR+'X_train.npy', X_train)
+    np.save(PATH_DATA_DIR+'y_train.npy', y_train)
+    np.save(PATH_DATA_DIR+'X_test.npy', X_test)
+    np.save(PATH_DATA_DIR+'y_test.npy', y_test)
+     
+
+def main():
+    read_tif(save_pickle=True)             # 12.9 GB
+    trim_and_split_data(save_pickle=True)  # 9.5 GB
+    load_data_from_pickles(PATH_PICKLE+'X_data.pickle', PATH_PICKLE+'y_data.pickle')
+
+main()
---
+++ utils.py
@@ -0,0 +1,250 @@
+
+import pickle
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from sklearn.model_selection import train_test_split
+
+#### Dataset ####
+
+class MyDataset(Dataset):
+    def __init__(self, images, labels, norm=False):
+        self.images = images
+        self.labels = labels
+        self.norm = norm
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        # Get image
+        img = self.images[index,:,:,:] 
+        if self.norm:  # normalize 
+            img = (img - np.mean(img))/np.std(img) # normalize to std normal dist
+        image = np.array(img, dtype=np.float32)
+        # Get labels
+        label = self.labels[index]
+        mask = np.zeros((3, 1, 1), dtype=np.float32) # One-hot encode label 
+        if label == '': mask[0,0] = 1
+        elif label == '': mask[1,0] = 1
+        elif label == '': mask[2,0] = 1
+        return image, mask
+    
+def check_inputs(train_ds, train_loader, savefig=False, name=None):
+    ''' 
+    Check data is loaded correctly
+    '''
+    print('Train data:')
+    print(f'     {len(train_ds)} obs, broken into {len(train_loader)} batches')
+    train_features, train_labels = next(iter(train_loader))
+    print(train_features.dtype)
+    shape = train_features.size()
+    print(f'     Each batch has data of shape {train_features.size()}, e.g. {shape[0]} images, {[shape[2], shape[3]]} pixels each, {shape[1]} layers (features)')
+    shape = train_labels.size()
+    print(shape)
+    print(f'     Each batch has labels of shape {train_labels.size()}, e.g. {shape[0]} images, {[shape[2], shape[3]]} pixels each, {shape[1]} layers (classes)')
+    if savefig:
+        fig, axs = plt.subplots(4, 1)
+        axs[0].set_title('images')
+        for i in range(0, 8, 2):
+            X, y = next(iter(train_loader))
+            im1 = axs[i].imshow(X[0,0,:,:]); plt.colorbar(im1, ax=axs[i]) # first img in batch, first channel
+        plt.savefig(f'traindata_{name}')
+
+#### Models ####
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_chans, out_chans):
+        super(DoubleConv, self).__init__()
+        self.conv1 = nn.Conv2d(in_chans, out_chans, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_chans, out_chans, kernel_size=3, stride=1, padding=1)
+        self.batchnorm = nn.BatchNorm2d(out_chans)
+        self.relu  = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, X):
+        X = self.relu(self.batchnorm(self.conv1(X)))
+        X = self.relu(self.batchnorm(self.conv2(X)))
+        X = self.maxpool(X)
+        return X
+    
+class TripleConv(nn.Module):
+    def __init__(self, in_chans, out_chans):
+        super(TripleConv, self).__init__()
+        self.conv1 = nn.Conv2d(in_chans, out_chans, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_chans, out_chans, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(out_chans, out_chans, kernel_size=3, stride=1, padding=1)
+        self.batchnorm = nn.BatchNorm2d(out_chans)
+        self.relu  = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, X):
+        X = self.relu(self.batchnorm(self.conv1(X)))
+        X = self.relu(self.batchnorm(self.conv2(X)))
+        X = self.relu(self.batchnorm(self.conv3(X)))
+        X = self.maxpool(X)
+        return X
+    
+class MyVGG16(nn.Module):
+    def __init__(self, num_classes=3):
+        super(MyVGG16, self).__init__() 
+        self.convs = nn.Sequential(
+            DoubleConv(3, 64), 
+            DoubleConv(64, 128), 
+            TripleConv(128, 256), 
+            TripleConv(256, 512), 
+            TripleConv(512, 512))
+        self.linear1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 2048), # 4096
+            nn.ReLU())
+        self.linear2 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(2048, 2048), # 4096
+            nn.ReLU())
+        self.linear3 = nn.Sequential(
+            nn.Linear(2048, num_classes)) # 4096
+        
+    def forward(self, X):
+        print(X.shape)
+        out = self.convs(X)
+        print(out.shape)
+        out = out.reshape(out.size(0), -1)
+        print(out.shape)
+        out = self.linear1(out)
+        print(out.shape)
+        out = self.linear2(out)
+        print(out.shape)
+        out = self.linear3(out)
+        print(out.shape)
+        return out
+    
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+        super(ResBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU())
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+class MyResNet(nn.Module):
+    def __init__(self, layers, num_classes = 10):
+        super(MyResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.layer0 = self._make_layer(ResBlock, 64, layers[0], stride = 1)
+        self.layer1 = self._make_layer(ResBlock, 128, layers[1], stride = 2)
+        self.layer2 = self._make_layer(ResBlock, 256, layers[2], stride = 2)
+        self.layer3 = self._make_layer(ResBlock, 512, layers[3], stride = 2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512, num_classes)
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes))
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+#### Training ####
+ 
+def train(train_loader, model, loss_fn, optimizer, device):
+    '''
+    Train one epoch
+    '''
+    for i, (images, labels) in enumerate(train_loader):  
+        images = images.to(device)
+        labels = labels.to(device)
+        # Forward pass
+        outputs = model(images)
+        loss = loss_fn(outputs, labels)
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+def validate(loader, model, device):
+    '''
+    Compute results on validation set
+    '''
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+        accuracy = correct/total
+        return accuracy
+
+
+#### Misc ####
+
+def load_data_from_pickles(imfile, labelfile):
+    '''
+    Process the data in the pickle files into train/test/val sets
+    Save as numpy files
+    '''
+    # Get data from pickles
+    with open(imfile, 'rb') as filename:
+            images = pickle.load(filename)
+    with open(labelfile, 'rb') as filename:
+            labels = pickle.load(filename)
+    # Split into train and test
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=1)
+    np.save('Data/X_train.npy', X_train)
+    np.save('Data/y_train.npy', y_train)
+    np.save('Data/X_test.npy', X_test)
+    np.save('Data/y_test.npy', y_test)
+    np.save('Data/X_val.npy', X_val)
+    np.save('Data/y_val.npy', y_val)
